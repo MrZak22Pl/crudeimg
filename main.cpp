58635a62f9bbe3c5c960354b9b2db25069eceaf4
MrZak22Pl <zak22pl@gmail.com>
2021-05-26 00:44:43 +0200

    Added smooth scrolling

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <SFML/Graphics.hpp>
+#include <time.h>
 
 float calc_scale(const sf::Vector2f &s1,const sf::Vector2f &s2){
     return s1.x/s2.x > s1.y/s2.y ? s1.y/s2.y:s1.x/s2.x;
+}
+
+float lerp(float v0, float v1, float t) {
+  return (1 - t) * v0 + t * v1;
+}
+
+sf::Vector2f lerp_vec(sf::Vector2f v0, sf::Vector2f v1, float t) {
+  return sf::Vector2f(lerp(v0.x,v1.x,t),lerp(v0.y,v1.y,t));
 }
 
 int main(int argc, char **argv){
@@ -9,8 +18,12 @@ int main(int argc, char **argv){
     sf::RenderWindow window;
     sf::Texture image;
     sf::Vector2f img_size;
-    sf::Vector2f mouse_position,old_mouse_position,camera_pos;
+    sf::Vector2f mouse_position,old_mouse_position,camera_pos,lerp_cam_pos;
     float scale = 1.0;
+    float lerp_scale = 1.0;
+
+    double start,frame_time;
+
     bool moved = 0;
 
     if(argc > 1){
@@ -24,34 +37,36 @@ int main(int argc, char **argv){
     window.setFramerateLimit(60);
 
     sf::Sprite sprite(image);
-
+    sprite.setOrigin(sf::Vector2f(image.getSize().x/2.0,image.getSize().y/2.0));
     while(window.isOpen()){
         
+        start = clock();
+        moved = 0;
+
         old_mouse_position = mouse_position;
         mouse_position = sf::Vector2f(sf::Mouse::getPosition(window));
 
         sf::Event event;
 
-        moved = 0;
-
         while(window.pollEvent(event)){
+            if(event.type == sf::Event::Closed){window.close();}
 
             if(event.type == sf::Event::Resized){
                 window.setView(sf::View(sf::FloatRect(0,0,event.size.width,event.size.height)));
                 scale = calc_scale(sf::Vector2f(window.getSize()),sf::Vector2f(image.getSize()));
             }
 
-            if(event.type == sf::Event::Closed){window.close();}
             if(event.type == sf::Event::MouseMoved){moved = 1;}
 
             if(event.type == sf::Event::MouseWheelScrolled){
 
-                sf::Vector2f fac((camera_pos.x - window.getSize().x/2.0) / (image.getSize().x*scale),( camera_pos.y - window.getSize().y/2.0) / (image.getSize().y*scale));
+                 sf::Vector2f fac((camera_pos.x - mouse_position.x) / (image.getSize().x*scale),( camera_pos.y -  mouse_position.y) / (image.getSize().y*scale));
 
                 scale += scale*0.2*event.mouseWheelScroll.delta;
 
-                camera_pos.x = (fac.x * (image.getSize().x*scale))+window.getSize().x/2.0;
-                camera_pos.y = (fac.y * (image.getSize().y*scale))+window.getSize().y/2.0;
+                camera_pos.x = (fac.x * (image.getSize().x*scale))+( mouse_position.x);
+                camera_pos.y = (fac.y * (image.getSize().y*scale))+( mouse_position.y);
+                
             }
         }
 
@@ -59,34 +74,38 @@ int main(int argc, char **argv){
 
         if(sf::Mouse::isButtonPressed(sf::Mouse::Left) && moved){
             camera_pos += mouse_position - old_mouse_position;
+            lerp_cam_pos = camera_pos;
         }
 
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::LShift)){
-            if(sf::Keyboard::isKeyPressed(sf::Keyboard::R)){
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::LShift) && sf::Keyboard::isKeyPressed(sf::Keyboard::R)){
                scale = calc_scale(sf::Vector2f(window.getSize()),sf::Vector2f(image.getSize()));
-            }
         }
         
-        if(camera_pos.x > 0){camera_pos.x = 0;}
-        if(camera_pos.y > 0){camera_pos.y = 0;}
+        if(camera_pos.x > img_size.x/2.0){camera_pos.x = img_size.x/2.0;}
+        if(camera_pos.y > img_size.y/2.0){camera_pos.y = img_size.y/2.0;}
 
-        if(camera_pos.x < window.getSize().x - img_size.x){camera_pos.x = window.getSize().x - img_size.x;}
-        if(camera_pos.y < window.getSize().y - img_size.y){camera_pos.y = window.getSize().y - img_size.y;}
+        if(camera_pos.x < window.getSize().x - img_size.x/2.0){camera_pos.x = window.getSize().x - img_size.x/2.0;}
+        if(camera_pos.y < window.getSize().y - img_size.y/2.0){camera_pos.y = window.getSize().y - img_size.y/2.0;}
 
         if(window.getSize().x >= img_size.x ){
-            camera_pos.x = window.getSize().x/2.0 - img_size.x/2.0;
+            camera_pos.x = window.getSize().x/2.0; 
         }
 
         if(window.getSize().y >= img_size.y){
-            camera_pos.y = window.getSize().y/2.0 - img_size.y/2.0;
+            camera_pos.y = window.getSize().y/2.0;
         }
+        
+        lerp_scale = lerp(lerp_scale,scale,frame_time);
+        lerp_cam_pos = lerp_vec(lerp_cam_pos,camera_pos,frame_time);
 
-        sprite.setScale(sf::Vector2f(scale,scale));
-        sprite.setPosition(camera_pos);
+        sprite.setScale(sf::Vector2f(lerp_scale,lerp_scale));
+        sprite.setPosition(lerp_cam_pos);
 
         window.clear();
         window.draw(sprite);
         window.display();
+
+        frame_time = (clock() - start)/1000.0;
     }
 
     return 0;
